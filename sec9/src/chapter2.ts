ee94f6652f386602803b1d3e212f9468c05e2b1b
summermong <2514o_o@naver.com>
2023-12-30 02:02:12 +0900

    FEAT: 조건부 타입

--- sec9/src/chapter2.ts
+++ sec9/src/chapter2.ts
@@ -9,5 +9,19 @@ type ReturnType<T> = T extends () => infer R ? R : never;
 
 type A = ReturnType<FuncA>;
 type B = ReturnType<FuncB>;
-
 type C = ReturnType<number>;
+
+
+// type PromiseUnpack<T> = any;
+
+type PromiseUnpack<T> = T extends Promise<infer R> ? R : never;
+
+// T는 Promise 타입이어야 한다.
+// Promise 타입의 결과값 타입을 반환해야 한다.
+
+type PromiseA = PromiseUnpack<Promise<number>>;
+// number;
+
+type PromiseB = PromiseUnpack<Promise<string>>;
+// string;
+
