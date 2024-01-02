8992c1092d7f4bdfc83f5c3a970037a1e39f024b
summermong <2514o_o@naver.com>
2024-01-02 17:09:27 +0900

    FEAT: 상태관리와 props 1

--- sec11/src/App.tsx
+++ sec11/src/App.tsx
@@ -1,7 +1,43 @@
 import './App.css';
+import { useState, useRef, useEffect } from 'react';
+import Editor from './components/Editor';
+
+interface Todo {
+  id: number;
+  content: string;
+}
 
 function App() {
-  return <div className="App"></div>;
+  // setText는 제네릭 함수
+  // useState에 아무것도 안 넣으면 undefined로 추론됨
+  // 넣을 게 없으면 useState<string>() 이런 식으로 정해줘야 함
+  // 그래서 초기값을 넣는 게 더 낫다
+
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const idRef = useRef(0);
+
+  const onClickAdd = (text: string) => {
+    setTodos([
+      ...todos,
+      {
+        id: idRef.current++,
+        content: text,
+      },
+    ]);
+  };
+
+  useEffect(() => {
+    console.log(todos);
+  }, [todos]);
+
+  return (
+    <div className="App">
+      <h1>To do</h1>
+      <Editor onClickAdd={onClickAdd} />
+      {/*<div>child</div>*/}
+    </div>
+  );
 }
 
 export default App;
